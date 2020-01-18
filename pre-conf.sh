dda35d086db441070393cd0fd7209b2a8f0aee9e
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-01-18 11:07:51 -0500

    Update Nagios to 4.4.5
    Update Nagios to 4.4.5
    Update nagios-plugins to 2.3.1
    Update nrpe to 3.3.0

--- pre-conf.sh
+++ pre-conf.sh
@@ -10,15 +10,16 @@
  usermod -a -G nagcmd www-data
  usermod -G nagios www-data
  cd /tmp
- wget https://sourceforge.net/projects/nagios/files/nagios-4.x/nagios-4.4.3/nagios-4.4.3.tar.gz
- wget http://nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz
- wget https://sourceforge.net/projects/nagios/files/nrpe-3.x/nrpe-3.2.1/nrpe-3.2.1.tar.gz
- tar -xvf nagios-4.4.3.tar.gz
- tar -xvf nagios-plugins-2.2.1.tar.gz
- tar -xvf nrpe-3.2.1.tar.gz
-
+ wget https://sourceforge.net/projects/nagios/files/nagios-4.x/nagios-4.4.5/nagios-4.4.5.tar.gz
+ wget http://nagios-plugins.org/download/nagios-plugins-2.3.1.tar.gz
+ wget https://sourceforge.net/projects/nagios/files/nrpe-3.x/nrpe-3.3.0/nrpe-3.3.0.tar.gz
+ tar -xvf nagios-4.4.5.tar.gz
+ tar -xvf nagios-plugins-2.3.1.tar.gz
+ tar -xvf nrpe-3.3.0.tar.gz
+ rm nagios-4.4.5.tar.gz nagios-plugins-2.3.1.tar.gz nrpe-3.3.0.tar.gz
+ 
  #installing nagios
- cd /tmp/nagios-4.4.3
+ cd /tmp/nagios-4.4.5
   ./configure --with-nagios-group=nagios --with-command-group=nagcmd --with-mail=/usr/sbin/sendmail --with-httpd_conf=/etc/apache2/conf-available
   make all
   make install
@@ -35,12 +36,12 @@
   ln -s /etc/init.d/nagios /etc/rcS.d/S99nagios
   
   #installing plugins 
-  cd /tmp/nagios-plugins-2.2.1/
+  cd /tmp/nagios-plugins-2.3.1/
   ./configure --with-nagios-user=nagios --with-nagios-group=nagios --enable-perl-modules --enable-extra-opts
   make
   make install
 
-  cd /tmp/nrpe-3.2.1/
+  cd /tmp/nrpe-3.3.0/
   ./configure --with-nrpe-user=nagios --with-nrpe-group=nagios --with-nagios-user=nagios --with-nagios-group=nagios  --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
   make all
   make install-plugin
